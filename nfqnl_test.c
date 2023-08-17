25e5e1105527244bd7f1cd5f729efa7033ac686c
juwon334 <juwon334@gmail.com>
2023-08-17 16:03:55 +0900

    0817*2

--- nfqnl_test.c
+++ nfqnl_test.c
@@ -1,12 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <linux/types.h>
-#include <linux/netfilter.h>		/* for NF_ACCEPT */
-#include <errno.h>
-#include <string.h>
-#include <libnetfilter_queue/libnetfilter_queue.h>
 #include "nfqnl_test.h"
 void dump(unsigned char* buf, int size) {
 	int i;
